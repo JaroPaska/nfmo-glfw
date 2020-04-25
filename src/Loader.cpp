840909d545509571a1c1d86ab66f9d8b6ff0032d
Phyrexian <jarko.paska@gmail.com>
2020-04-25 18:08:23 +0200

    undo some shit

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -29,20 +29,6 @@ void Loader::loadModel(std::shared_ptr<Model> model) {
                     for (int m = 0; m < 3; m++)
                         buffer.push_back(byPolyType[i][j]->color[m] / 255.f);
                 }
-                /*std::reverse(byPolyType[i][j]->triangles[k].begin(),
-                             byPolyType[i][j]->triangles[k].end());
-                byPolyType[i][j]->normal = -byPolyType[i][j]->normal;
-                for (int l = 0; l < 3; l++) {
-                    for (int m = 0; m < 3; m++)
-                        buffer.push_back(model->points[byPolyType[i][j]->triangles[k][l]][m]);
-                    for (int m = 0; m < 3; m++)
-                        buffer.push_back(byPolyType[i][j]->normal[m]);
-                    for (int m = 0; m < 3; m++)
-                        buffer.push_back(byPolyType[i][j]->color[m] / 255.f);
-                }
-                std::reverse(byPolyType[i][j]->triangles[k].begin(),
-                             byPolyType[i][j]->triangles[k].end());
-                byPolyType[i][j]->normal = -byPolyType[i][j]->normal;*/
             }
         }
         if (!buffer.empty()) {
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -51,9 +51,9 @@ void Renderer::render(std::shared_ptr<Stage> stage) {
 
     //
     glClear(GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_CULL_FACE);
+    /*glEnable(GL_CULL_FACE);
     glFrontFace(GL_CCW);
-    glCullFace(GL_BACK);
+    glCullFace(GL_BACK);*/
     glEnable(GL_DEPTH_TEST);
     polyShader->use();
     polyShader->setMat4("uni_projection", projection);
