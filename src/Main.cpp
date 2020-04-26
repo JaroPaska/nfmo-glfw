98ebc0d4b80ebd5638a32d6f14357af1e61a2f40
Jozef Číž <jozef.ciz.ml@gmail.com>
2020-04-26 15:56:59 +0200

    camera rotation speed set to bearable level

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,9 +18,9 @@ int main() {
         if (Window::getKey(GLFW_KEY_ENTER))
             Renderer::camera->pos.y += 10.f;
         if (Window::getKey(GLFW_KEY_LEFT))
-            Renderer::camera->rot = glm::rotate(Renderer::camera->rot, -0.01f, Y_AXIS);
+            Renderer::camera->rot = glm::rotate(Renderer::camera->rot, -0.04f, Y_AXIS);
         if (Window::getKey(GLFW_KEY_RIGHT))
-            Renderer::camera->rot = glm::rotate(Renderer::camera->rot, 0.01f, Y_AXIS);
+            Renderer::camera->rot = glm::rotate(Renderer::camera->rot, 0.04f, Y_AXIS);
         if (Window::getKey(GLFW_KEY_UP)) {
             glm::vec3 v = glm::vec3(glm::mat4(Renderer::camera->rot) * glm::vec4(0, 0, 100, 1));
             v.x = -v.x; // why the fuck is this necessary
